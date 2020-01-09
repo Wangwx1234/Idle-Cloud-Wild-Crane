8173bc21617d909dc828f83629abb9b4f6676ebd
wangwenxian <wangwenxian@ispiderman.com>
2020-01-09 13:41:14 +0800

    创建项目eureka,admin,config

---
+++ config/src/main/java/com/xianyun/stu/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.xianyun.stu.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
---
+++ config/src/test/java/com/xianyun/stu/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xianyun.stu.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
